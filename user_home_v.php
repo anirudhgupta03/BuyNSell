eaf4da1f415f131dafaf1955ffda3a402b658297
Vishwam Shriram Mundada <58394139+Vishwam36@users.noreply.github.com>
2023-05-09 23:15:02 +0530

    yet another user_home

---
+++ user_home_v.php
@@ -0,0 +1,223 @@
+<?php 
+session_start();
+include('db.php');
+// include('pro_table_check.php');
+
+if(isset($_SESSION['user'])) {
+    $row_c = $_SESSION['user'];
+}
+else if(isset($_SESSION['admin_login'])) {
+    $row_c = $_SESSION['admin_login'];
+	header("location:admin_home.php");
+}
+else {
+    header("location:index.php");
+}
+
+$home = true;
+$view = false;
+$bids = false;
+$products = false;
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+
+	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+	<link rel = "stylesheet" href = "style.css">
+	<title> User Home </title>
+	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+	<link rel = "stylesheet" href = "style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">	
+</head>
+<?php include 'head.php'; ?>
+
+<style>
+/*
+body {
+	background-image: url(Silver-Blur-Background-Wallpaper.jpg);
+	background-repeat: no-repeat;
+	background-size: cover;
+	background-attachment: fixed;
+}*/
+
+.bg-gray {
+    background-color: rgba(24, 44, 97, .3);
+}
+.container_flex {
+	background-color: gray;
+	display: flex;
+	flex-direction: row-reverse;
+}
+
+.bg-nav {
+    background: -webkit-linear-gradient(left, #a445b2, #fa4299); !important;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    z-index: 5;
+}
+
+.bg-darkblue {
+    background-color: rgb(24, 44, 97) !important;
+}
+
+.item {
+	margin: 0;
+}
+
+.product_img {
+	background-size: cover;
+}
+
+/*.card {
+	width: 30%;
+}*/
+
+.row {
+	display: flex;
+	align-items: stretch;
+}
+
+.max-width {
+	max-width: 30%;
+}
+
+.bg-card {
+	background-color: rgba(189, 195, 199, .7);/*
+	background-color: rgba(112, 111, 211, .7);*/
+	color: #6c757d !important;
+}
+
+
+.bg-card-footer {
+	background-color: rgba(236, 240, 241, .5);/*
+	background-color: rgba(39, 60, 117, .7);*/
+}
+
+.mt-5, .my-5 {
+    margin-top: 4rem!important;
+}
+.text {
+  color: #6c757d !important;
+}
+
+a.text:hover,
+a.text:focus {
+  color: #57606f !important;
+}
+
+</style>
+
+<body>
+
+	<?php
+if (isset($_SESSION['user'])){
+?>
+<nav class="navbar navbar-expand-sm navbar-dark bg-nav animated fadeInDown">
+		<div class="container">
+
+			<a style="color: #ffc107;" class="navbar-brand" href="index.php">
+				<img style="max-width:190px; margin-top: -1px;" src="logo.png">
+			</a>
+
+			
+			<div class="search-box">
+            	<form action = "searchresult.php" method="POST" class = "search-bar" autocomplete = "off">
+              		<!-- <div class="control-group" style="display:flex;"> -->
+                		<input type = "text" name = "search" placeholder="Search here..." required />
+                		<button type="submit"><img src = "images/search.png"> </button> 
+              		<!-- </div> -->
+            	</form>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+
+				<div class="nav-item dropdown">
+					
+					<a href="#" class="nav-link dropdown-toggle text-white" data-toggle="dropdown"><?php echo $row_c->name;?></a>
+					<div class="dropdown-menu bg-darkblue">
+						<a href="view_profile.php" class="text-warning dropdown-item ">View Profile</a>
+						<a href="seller_rating.php" class="text-warning dropdown-item ">My Rating</a>
+						<a href="wishlist.php" class="text-warning dropdown-item ?>">Products in my Wishlisht </a>
+						<a href="product.php" class="text-warning dropdown-item ">Products I put for Sale</a>
+						<a href="got.php" class="text-warning dropdown-item ">Products I Purchased!!</a>
+					</div>
+				</div>&nbsp;&nbsp;&nbsp;	
+				<div class = "nav-item">
+				<a class="btn btn-warning" href="add_product.php">Add A Product To Sell</a>
+				</div>&nbsp;&nbsp;&nbsp;	
+				<div class="nav-item">
+					<a class="btn btn-danger <?php echo 'active';?>" href="logout.php">Logout</a>
+				</div>
+          	</div>
+		</div>
+	</nav>
+	<?php
+}
+?>
+		
+<br><br>
+
+    <?php
+    $query1 = "select * from product_category";
+	$run_q1 = $con->query($query1);
+	$showing_products = $run_q1->num_rows;
+    ?>
+
+    <!-- <h4 class="m-3 text-info">Showing&nbsp;Products&nbsp;Category</h4> -->
+
+    <form>
+		    <div class="container mt-5 mb-5">
+				<?php
+
+				?>
+				<div class="row">
+				<?php
+				
+				while ($row_q1 = $run_q1->fetch_object()) {
+						?>
+							<div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">	
+								<div class="card mt-3 mb-3" style="border-radius:15px 15px 15px 15px;">
+									<?php  
+									// $query6 = "select * from tbl_img where pro_id = $pro_id LIMIT 1";
+									// $run_q6 = $con->query($query6);
+									// $row_q6 = $run_q6->fetch_object();
+									$image_name = $row_q1->img_name;
+									$image_destination = "product_category/".$image_name;
+									$dest = strtolower($row_q1->name).".php";
+									?>
+									<a href="<?php echo $dest; ?>">
+										<img class="product_img card-img-top" style="border-radius:15px 15px 0px 0px;" src="<?php echo $image_destination; ?>"  height="200vh" width="100%" alt="Product Image">
+									</a>
+									<div class="card-body" style="border-radius:0px 0px 15px 15px; background: linear-gradient(to right, rgb(242, 112, 156), rgb(255, 148, 114));">
+										<a class="card-title text-dark" style="border-radius:15px 15px 0px 0px;" href="<?php echo $dest; ?>"><h5 style="color:white;"><?php echo $row_q1->name; ?></h5></a>
+									</div>
+								</div>
+							</div>
+						<?php
+				}
+				?>
+				</div>
+			</div>
+	</form>
+
+	
+	
+
+	
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
+</html>
